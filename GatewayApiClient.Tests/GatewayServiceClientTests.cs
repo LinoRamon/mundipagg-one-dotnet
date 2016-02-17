59ff35b5c2e137784d9ef8c843ab62b39403233e
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-17 12:26:53 -0200

    :white_check_mark: ONEPROJECT-603 Adiciona testes para o get buyer

--- GatewayApiClient.Tests/GatewayServiceClientTests.cs
+++ GatewayApiClient.Tests/GatewayServiceClientTests.cs
@@ -60,6 +60,39 @@ namespace GatewayApiClient.Tests {
             AmountInCents = 100,
             Options = new CreditCardTransactionOptions { PaymentMethodCode = 1 }
         };
+
+        private readonly CreateBuyerRequest _createBuyer = new CreateBuyerRequest {
+            Birthdate = new DateTime(1994, 9, 26, 10, 35, 12),
+            BuyerCategory = BuyerCategoryEnum.Normal,
+            BuyerReference = "DotNet Buyer",
+            CreateDateInMerchant = DateTime.UtcNow.AddDays(-5),
+            DocumentNumber = "12345678901",
+            DocumentType = DocumentTypeEnum.CPF,
+            Email = "dotnet@developer.com",
+            EmailType = EmailTypeEnum.Personal,
+            FacebookId = "developer.net",
+            Gender = GenderEnum.M,
+            HomePhone = "2125247689",
+            IpAddress = "192.168.0.1",
+            LastBuyerUpdateInMerchant = DateTime.UtcNow.AddDays(-2),
+            MobilePhone = "21989685642",
+            Name = "Dotnet Developer",
+            PersonType = PersonTypeEnum.Person,
+            TwitterId = "@developer.net",
+            WorkPhone = "21965647826",
+            AddressCollection = new Collection<BuyerAddress> { new BuyerAddress
+                {
+                    AddressType = AddressTypeEnum.Residential,
+                    City = "Rio de Janeiro",
+                    Complement = "Aeroporto",
+                    Country = "Brazil",
+                    District = "Centro",
+                    Number = "123",
+                    State = "RJ",
+                    Street = "Av. General Justo",
+                    ZipCode = "20270230"
+                }}
+        };
         #endregion
 
 
@@ -344,41 +377,18 @@ namespace GatewayApiClient.Tests {
 
         [TestMethod]
         public void ItShouldCreateBuyer() {
-            CreateBuyerRequest createBuyer = new CreateBuyerRequest {
-                Birthdate = DateTime.ParseExact("1994-09-26", "yyyy-MM-dd", null),
-                BuyerCategory = BuyerCategoryEnum.Normal,
-                BuyerReference = "DotNet Buyer",
-                CreateDateInMerchant = DateTime.UtcNow.AddDays(-5),
-                DocumentNumber = "12345678901",
-                DocumentType = DocumentTypeEnum.CPF,
-                Email = "dotnet@developer.com",
-                EmailType = EmailTypeEnum.Personal,
-                FacebookId = "developer.net",
-                Gender = GenderEnum.M,
-                HomePhone = "2125247689",
-                IpAddress = "192.168.0.1",
-                LastBuyerUpdateInMerchant = DateTime.UtcNow.AddDays(-2),
-                MobilePhone = "21989685642",
-                Name = "Dotnet Developer",
-                PersonType = PersonTypeEnum.Person,
-                TwitterId = "@developer.net",
-                WorkPhone = "21965647826",
-                AddressCollection = new Collection<BuyerAddress> { new BuyerAddress
-                {
-                    AddressType = AddressTypeEnum.Residential,
-                    City = "Rio de Janeiro",
-                    Complement = "Aeroporto",
-                    Country = "Brazil",
-                    District = "Centro",
-                    Number = "123",
-                    State = "RJ",
-                    Street = "Av. General Justo",
-                    ZipCode = "20270230"
-                }}
-            };
-
             var client = new GatewayServiceClient(Guid.Parse("8A2DD57F-1ED9-4153-B4CE-69683EFADAD5"), new Uri("https://stagingv2.mundipaggone.com"));
-            var response = client.Buyer.CreateBuyer(createBuyer);
+            var response = client.Buyer.CreateBuyer(this._createBuyer);
+
+            Assert.IsTrue(response.Response.Success);
+        }
+
+        [TestMethod]
+        public void ItShouldGetBuyer() {
+            var client = new GatewayServiceClient(Guid.Parse("8A2DD57F-1ED9-4153-B4CE-69683EFADAD5"), new Uri("https://stagingv2.mundipaggone.com"));
+            Guid buyerKey = client.Buyer.CreateBuyer(this._createBuyer).Response.BuyerKey;
+
+            var response = client.Buyer.GetBuyer(buyerKey);
 
             Assert.IsTrue(response.Response.Success);
         }
--- ResourceClients/BuyerResource.cs
+++ ResourceClients/BuyerResource.cs
@@ -5,14 +5,11 @@ using GatewayApiClient.EnumTypes;
 using GatewayApiClient.ResourceClients.Interfaces;
 using GatewayApiClient.Utility;
 
-namespace GatewayApiClient.ResourceClients
-{
-    public class BuyerResource : BaseResource, IBuyerResource
-    {
+namespace GatewayApiClient.ResourceClients {
+    public class BuyerResource : BaseResource, IBuyerResource {
         public BuyerResource(Guid merchantKey, Uri hostUri, NameValueCollection customHeaders) : base(merchantKey, "/Buyer", hostUri, customHeaders) { }
 
-        public HttpResponse<GetBuyerData> GetBuyer(Guid buyerKey)
-        {
+        public HttpResponse<GetBuyerData> GetBuyer(Guid buyerKey) {
             string actionName = string.Format("/{0}", buyerKey.ToString());
 
             HttpVerbEnum httpVerb = HttpVerbEnum.Get;
@@ -23,8 +20,7 @@ namespace GatewayApiClient.ResourceClients
             return this.HttpUtility.SubmitRequest<GetBuyerData>(string.Concat(this.HostUri, this.ResourceName, actionName), httpVerb, HttpContentTypeEnum.Json, headers);
         }
 
-        public HttpResponse<CreateBuyerResponse> CreateBuyer(CreateBuyerRequest createBuyerRequest)
-        {
+        public HttpResponse<CreateBuyerResponse> CreateBuyer(CreateBuyerRequest createBuyerRequest) {
             HttpVerbEnum httpVerb = HttpVerbEnum.Post;
 
             NameValueCollection headers = this.GetHeaders();
