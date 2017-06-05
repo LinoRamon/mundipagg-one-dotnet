76aded6fb9f8658cdfc1fc3c13c6826c5d05d26f
= <=>
2017-06-05 19:03:42 -0300

    :art: Ajusta summary

--- DataContracts/OnlineDebitTransaction/OnlineDebitTransactionOptions.cs
+++ DataContracts/OnlineDebitTransaction/OnlineDebitTransactionOptions.cs
@@ -4,7 +4,7 @@ namespace GatewayApiClient.DataContracts.OnlineDebitTransaction {
     [DataContract(Name = "OnlineDebitTransactionOptions", Namespace = "")]
     public class OnlineDebitTransactionOptions {
         /// <summary>
-        /// Define se o pagamento será realizado com dinheiro físico.
+        /// Define se o pagamento será realizado com dinheiro físico(SafetyPay).
         /// </summary>
         [DataMember]
         public bool IsCashTransaction { get; set; }
